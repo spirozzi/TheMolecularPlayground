25b7c9127f3f491529a6815b595e34a571cefcd9
Carter Tiernan <ctiernan@umass.edu>
2015-12-08 20:08:44 -0500

    working front end login functionality

--- src/public/controllers/contentCntrl.js
+++ src/public/controllers/contentCntrl.js
@@ -2,9 +2,21 @@
 
 var content = angular.module('content', [])
 
-content.directive('content', function() {
+content.controller('contentCntrl', ['$rootScope','$scope', function($rootScope,$scope) {
+  $rootScope.logged_in = 1;
+}]);
+
+content.directive('gallery', function() {
   return {
-    templateUrl: 'templates/content.ejs'
+    restrict: 'E',
+    templateUrl: 'templates/gallery.ejs'
   };
 });
+content.directive('home', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/home.ejs'
+  };
+});
+
 })();
--- src/public/controllers/navCntrl.js
+++ src/public/controllers/navCntrl.js
@@ -2,25 +2,25 @@
 
 var nav = angular.module('nav', [])
 
-nav.controller('navCntrl', ['$scope', function($scope) {
+nav.controller('navCntrl', ['$rootScope','$scope', function($rootScope,$scope) {
+
   var in_links = [
-    {text:'sign out',action:'nav.sign_in()'},
-    {text:'account',action:'link'},
-    {text:'gallery',action:'link'},
-    {text:'home',action:'link'}
+    {text:'Home',action:'link'},
+    {text:'Gallery',action:'link'},
+    {text:'Account',action:'link'},
+    {text:'Sign Out',action:'nav.sign_in()'}
   ];
   var out_links = [
     {text:'Sign Up',action:"openSignInUpModal()"},
     {text:'Log In',action:"openSignInUpModal()"}
   ];
 
-  this.sign_in = function(){
-    $scope.signed_in = !$scope.signed_in;
-  };
+  $rootScope.get_links = function(lin){
+    console.log(lin);
+    if($rootScope.logged_in) links = in_links;
+    else links = out_links;
 
-  this.get_links = function(){
-    if (this.signed_in) return in_links;
-    else return out_links;
+    return links;
   };
 
   this.showSignInUpModal = function(){
@@ -47,11 +47,4 @@ nav.controller('navCntrl', ['$scope', function($scope) {
     $('#signinupModal').openModal();
   };
 }]);
-
-nav.directive('navbar', function() {
-  return {
-    templateUrl: 'templates/nav.ejs',
-    controller: 'navCntrl as nav'
-  };
-});
 })();
